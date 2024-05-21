1ac717c9a5b9dd4b8da239355c12976a36f8d62d
Anudeep Yegireddi <anudeep@Anudeeps-MBP.lan>
2024-05-21 10:42:17 +0530

    feat: Implemented errorpage, loader, action
    Notes:
    - Error page implemented, with typing to handle `error.statusText ||
      error.message`
    - loader implemented, which is the get request that executes on the page
    - Child layout implmented, with contacts showing up without a full page
      rerender
    - Added an action, which is a repurposing of the Form in HTML. This
      takes the equivalent of post data and instead of sending a request to
      the sever, and sends it to our route whose config has been enhanced
      with an `action`
    	- Action takes care of refreshing the page and invalidating the
    	  loader

---
+++ src/routes/contact.tsx
@@ -0,0 +1,91 @@
+import { Form } from "react-router-dom";
+
+export type ContactType = {
+  id: string;
+  first: string;
+  last: string;
+  avatar?: string;
+  twitter?: string;
+  notes?: string;
+  favorite: boolean;
+};
+
+// export default function Contact(contact: Contact = defaultContact) {
+export default function Contact() {
+  const contact: ContactType = {
+    id: "0",
+    first: "Your",
+    last: "Name",
+    avatar: "https://picsum.photos/id/1/200/200",
+    twitter: "your_handle",
+    notes: "Some notes",
+    favorite: true,
+  };
+  return (
+    <div id="contact">
+      <div>
+        <img key={contact.avatar} src={contact.avatar || undefined} />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter && (
+          <p>
+            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
+              {contact.twitter}
+            </a>
+          </p>
+        )}
+
+        {contact.notes && <p>{contact.notes}</p>}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+          <Form
+            method="post"
+            action="destroy"
+            onSubmit={(event) => {
+              if (!confirm("Please confirm you want to delete this record.")) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type FavoriteProps = {
+  contact: ContactType;
+};
+
+function Favorite(props: FavoriteProps) {
+  // yes, this is a `let` for later
+  const favorite = props.contact.favorite;
+  return (
+    <Form method="post">
+      <button
+        name="favorite"
+        value={favorite ? "false" : "true"}
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </Form>
+  );
+}
---
+++ src/routes/root.tsx
@@ -0,0 +1,81 @@
+import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { createContact, getContacts } from "../contacts";
+import { ContactType } from "./contact";
+
+type LoaderData = {
+  contacts: ContactType[];
+};
+
+const loader = async (): Promise<LoaderData> => {
+  const contacts: ContactType[] = await getContacts();
+  return { contacts };
+};
+
+type ContactCreateType = {
+  id: string;
+  createdAt: number;
+};
+
+type ActionData = {
+  contact: ContactCreateType;
+};
+
+const action = async (): Promise<ActionData> => {
+  const contact: ContactCreateType = await createContact();
+  return { contact };
+};
+
+function Root() {
+  const { contacts } = useLoaderData() as LoaderData;
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No contacts found</p>
+          )}
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default Root;
+export { loader, action };
